92c38879ebbd7e8d8ba5665d01321db7ad4708f9
Kateryna Kovshykova <k.kovshykova@gmail.com>
2022-11-13 17:40:31 +0200

    New build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,13 @@ import { TaskCommentsComponent } from './pages/task-comments/task-comments.compo
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/lists', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
   { path: 'angular-task-app', component: TaskViewComponent },
   { path: 'lists/:listId', component: TaskViewComponent },
   { path: 'lists/:listId/edit-task/:taskId', component: TaskViewComponent },
   { path: 'lists/:listId/description', component: ListDescriptionComponent },
-  { path: 'lists/:listId/tasks/:taskId/comments', component: TaskCommentsComponent },
+  { path: 'lists/:listId/tasks/:taskId/comments', component: TaskCommentsComponent }
 ];
 
 @NgModule({
